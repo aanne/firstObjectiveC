adf06c951a320b7cd6eba9f27e98a6a21e3406ca
charlie <526825550@qq.com>
2018-07-09 11:51:13 +0800

    init commit

--- firstObjectiveC/main.m
+++ firstObjectiveC/main.m
@@ -8,10 +8,29 @@
 
 #import <Foundation/Foundation.h>
 
+BOOL areIntsDifferent(int a,int b){
+    if(a==b){
+        return (NO);
+    }else{
+        return (YES);
+    }
+}
+
+NSString *boolString(BOOL b){
+    if(b==NO){
+        return (@"NO");
+    }else{
+        return (@"YES");
+    }
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        BOOL areTheyDifferent=areIntsDifferent(4,5);
+        NSLog(@"are %d and %d different? %@",4,5,boolString(areTheyDifferent));
     }
     return 0;
 }
+
